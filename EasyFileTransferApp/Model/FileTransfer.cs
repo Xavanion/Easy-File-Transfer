2af405e112494615b4a7c6d7af89259bdfe88ed2
Xavanion <barrettbrown2012@gmail.com>
2025-03-04 00:30:44 -0600

    Added UDP discovery & TCP file sending backend

---
+++ EasyFileTransferApp/Model/FileTransfer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Used to handle file transfers between users
+namespace EasyFileTransferApp.Model
+{
+    class FileTransfer
+    {
+        private const int FileTransferPort = 8081; // Port used for file transfers
+
+        // Send a file to a client in 8kb chunks
+        public async Task SendFileAsync(string filePath, string ipAddress)
+        {
+            TcpClient client = new TcpClient(ipAddress, FileTransferPort); // Connect to the client
+            NetworkStream stream = client.GetStream(); // Get the network stream for reading/writing
+            FileStream fileStream = File.OpenRead(filePath); // Open the file for reading
+
+            // Send the file name and size to the client
+            string fileName = Path.GetFileName(filePath); // Get the file name
+            byte[] fileNameData = Encoding.UTF8.GetBytes(fileName); // Convert the file name to a byte array
+            byte[] fileNameLength = BitConverter.GetBytes(fileNameData.Length); // Convert the file name size to a byte array
+            await stream.WriteAsync(fileNameLength, 0, fileNameLength.Length); // Send the file name size to the client
+            await stream.WriteAsync(fileNameData, 0, fileNameData.Length); // Send the file name to the client
+
+            byte[] fileSize = BitConverter.GetBytes(fileStream.Length); // Convert the file size to a byte array
+            await stream.WriteAsync(fileSize, 0, fileSize.Length); // Send the file size to the client
+
+            // Send File data in chunks
+            byte[] data = new byte[8192]; // Buffer to hold the file data
+            int bytesRead; // Number of bytes read from the file
+            while ((bytesRead = fileStream.Read(data, 0, data.Length)) > 0) // While there is still bytes to be read read in buffer and send to client
+            {
+                await stream.WriteAsync(data, 0, bytesRead); // Send the file data to the client
+            }
+        }
+
+        // Receive a file from a client in 8kb chunks
+        public async Task RecieveFileAsync(string saveDirectory)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, FileTransferPort); // Create a TCP listener to listen for incoming file transfers
+            listener.Start(); // Start listening for incoming connections
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync(); // Accept the incoming connection
+                NetworkStream stream = client.GetStream(); // Get the network stream for reading/writing
+
+                // Read the file name length from the client
+                byte[] fileNameLengthData = new byte[sizeof(int)]; // Buffer to hold the file name size
+                await stream.ReadAsync(fileNameLengthData, 0, fileNameLengthData.Length); // Read the file name size from the client
+                int fileNameLength = BitConverter.ToInt32(fileNameLengthData, 0); // Convert the file name size to an integer
+
+                // Read file name from the client
+                byte[] fileNameData = new byte[fileNameLength]; // Buffer to hold the file name
+                await stream.ReadAsync(fileNameData, 0, fileNameData.Length); // Read the file name from the client
+                string fileName = Encoding.UTF8.GetString(fileNameData); // Convert the file name to a string
+
+                // Read the file size from the client
+                byte[] fileSizeData = new byte[sizeof(long)]; // Buffer to hold the file size
+                await stream.ReadAsync(fileSizeData, 0, fileSizeData.Length); // Read the file size from the client
+                long fileSize = BitConverter.ToInt64(fileSizeData, 0); // Convert the file size to a long
+
+                // Create a file stream to save the file
+                string savePath = Path.Combine(saveDirectory, fileName); // Combine the save directory and file name to get the save path
+                FileStream fileStream = File.Create(savePath); // Create the file stream to save the file
+
+                // Receive file data in chunks
+                byte[] data = new byte[8192]; // Buffer to hold the file data
+                int bytesRead; // Number of bytes read from the client
+                long totalBytesRead = 0; // Total number of bytes read from the client
+                while (totalBytesRead < fileSize) // While there is still bytes to be read read in buffer and write to file
+                {
+                    bytesRead = await stream.ReadAsync(data, 0, data.Length); // Read the file data from the client
+                    await fileStream.WriteAsync(data, 0, bytesRead); // Write the file data to the file
+                    totalBytesRead += bytesRead; // Update the total number of bytes read
+                }
+            }
+                
+        }
+
+    }
+}
---
+++ EasyFileTransferApp/Model/NetworkDiscovery.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation.Peers;
+using System.Net.Sockets;
+using System.Net;
+
+// Used to discover clients on local network
+namespace EasyFileTransferApp.Model
+{
+    class NetworkDiscovery
+    {
+        private int BroadcastPort = 8080; // Port used for broadcasting messages
+        private UdpClient udpClient; // UDP client used to send and receive messages
+        private bool isRunning; // Flag to indicate if the listener is running
+
+        // Event that is triggered when a client is discovered
+        public event Action<string, string> ClientDiscovered = delegate { };
+
+
+        // Constructor
+        public NetworkDiscovery()
+        {
+            udpClient = new UdpClient();
+            udpClient.EnableBroadcast = true;
+        }
+
+
+        // Broadcast a message to all clients on the network
+        public async Task StartBroadcastingAsync(string username)
+        {
+            while (true)
+            {
+                string message = $"DISCOVER:{username}";
+                byte[] data = Encoding.UTF8.GetBytes(message); // Convert message to byte array
+                await udpClient.SendAsync(data, data.Length, new IPEndPoint(IPAddress.Broadcast, BroadcastPort)); // Broadcast message to all clients on the network
+                await Task.Delay(5000);
+            }
+        }
+
+        // Listen for incoming messages from clients
+        public async Task StartListeningAsync()
+        {
+            isRunning = true;
+            UdpClient listener = new UdpClient(BroadcastPort); // Create a UDP client to listen for incoming messages
+            while (isRunning)
+            {
+                UdpReceiveResult result = await listener.ReceiveAsync(); // Receive incoming message
+                string message = Encoding.UTF8.GetString(result.Buffer); // Convert message to string
+                if (message.StartsWith("DISCOVER:"))
+                {
+                    string username = message.Substring(9);
+                    ClientDiscovered(username, result.RemoteEndPoint.Address.ToString()); // Trigger event to notify that a client has been discovered
+                }
+            }
+        }
+
+
+        // Stop listening for incoming messages
+        public void StopListening()
+        {
+            isRunning = false;
+            udpClient.Close();
+        }
+    }
+}
